62968051885bb6a1261ae3d060437531e2cd9228
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-10-11 17:25:43 +0500

    chore: Use lua style code.

--- lua/autorun/client/concommands_v2.lua
+++ lua/autorun/client/concommands_v2.lua
@@ -8,7 +8,7 @@ end)
 local auto_jump = CreateClientConVar("auto_jump", "0", true, false, "Auto jump")
 hook.Add("CreateMove", "autojump", function(cmd)
 	if auto_jump:GetBool() then
-		if bit.band(cmd:GetButtons(), IN_JUMP) != 0 then
+		if bit.band(cmd:GetButtons(), IN_JUMP) ~= 0 then
 			if not LocalPlayer():IsOnGround() and LocalPlayer():GetMoveType() ~= MOVETYPE_NOCLIP and LocalPlayer():WaterLevel() <= 1 then
 				cmd:SetButtons(bit.band(cmd:GetButtons(), bit.bnot(IN_JUMP)))
 			end
