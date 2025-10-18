15718f7053c2451459ab1aaf1ac68c7a6f3a05bd
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-10-18 14:57:44 +0500

    feat: Add workshop addon download.

---
+++ lua/autorun/server/sv_workshop_dl.lua
@@ -0,0 +1,7 @@
+-- Add workshop content for the clients.
+
+resource.AddWorkshop("160250458") -- Wiremod
+
+--#region Petitioned addons
+resource.AddWorkshop("610669977") -- Recolorable CS:S Models
+--#endregion
