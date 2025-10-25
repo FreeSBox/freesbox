5f0a417a9a824ac9f8e5e08a31ae5929d7222025
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-10-25 22:09:59 +0500

    chore: Update workshop dl.

--- lua/autorun/server/sv_workshop_dl.lua
+++ lua/autorun/server/sv_workshop_dl.lua
@@ -3,8 +3,7 @@
 resource.AddWorkshop("160250458") -- Wiremod
 
 --#region Petitioned addons
-resource.AddWorkshop("610669977") -- Recolorable CS:S Models
 resource.AddWorkshop("771487490") -- [simfphys] LUA Vehicles - Base
 resource.AddWorkshop("831680603") -- [simfphys] Armed
-resource.AddWorkshop("128091208") -- M9K Heavy Weapons -- Please vote this out of existence
+resource.AddWorkshop("1360233031") -- Half-Life-SWEPs
 --#endregion
