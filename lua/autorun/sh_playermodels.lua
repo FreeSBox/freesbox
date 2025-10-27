d4187783baf58259f850bf83f878e4ede41a36fe
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-10-27 20:57:59 +0500

    feat: Added more playermodels.

---
+++ lua/autorun/sh_playermodels.lua
@@ -0,0 +1,5 @@
+-- Malicious compliance for more player models petition
+
+player_manager.AddValidModel("monk_tpose", "models/monk.mdl")
+player_manager.AddValidModel("hl2_player", "models/player.mdl")
+player_manager.AddValidModel("info_player_start", "models/editor/playerstart.mdl")
