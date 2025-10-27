1ca83c4619ae12e7dc4722a2abbd2d65141d0dbe
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-10-28 02:35:22 +0500

    refactor(LagDetect): New lag detect logic.
    Hopefully it's more reliably, and not just more trigger happy.
    But it will somewhat help, as it now freezes objects immediately,
    instead of waiting for `phys_timescale 0` to take effect.
    
    We should look into freezing penetrating props before this point
    through. This will even allow us to see the owner of those
    props, and tell everyone about that son of a bitch.

--- lua/autorun/server/sv_lagdetect.lua
+++ lua/autorun/server/sv_lagdetect.lua
@@ -1,21 +1,69 @@
-local threshold = 1.6
+local threshold = 30 -- tps
 local notification = "Big lag detected! Clean up in 1 minute."
 local seconds_before_cleanup = 60
+
+local num_frames = 3 -- the avarage of this number of frames is checked against the threashold
 
 local sv_hibernate_think = GetConVar("sv_hibernate_think")
 local last_ticktime = 0
 local clean_up_started = false
 
+local function freezeAllPlayerProps()
+	for _, ent in ipairs(ents.GetAll()) do
+		if IsValid(ent) and ent:CPPIGetOwner() then
+			local phys_object = ent:GetPhysicsObject()
+			if IsValid(phys_object) then
+				phys_object:EnableMotion(false)
+			end
+		end
+	end
+end
+
+hook.Add("PlayerSpawnObject", "lag_detect_stop_spawn", function (ply, model, skin)
+	if clean_up_started then
+		print(ply:GetName() .. " tried to spawn: \"" .. model .. "\" while lag cleanup was in progress")
+		return false
+	end
+end)
+
+local last_frames = {}
+
+-- Fill the structure with ideal data initially.
+for i = 1, num_frames do
+	last_frames[i] = 1/engine.TickInterval()
+end
+
+local function pushFramerate(framerate)
+	for i = 1, num_frames-1 do
+		last_frames[i] = last_frames[i+1]
+	end
+	last_frames[num_frames] = framerate
+end
+
+local function getAvarageFramerate()
+	local sum = 0
+	for i = 1, num_frames do
+		sum = sum + last_frames[i]
+	end
+	return sum/num_frames
+end
+
 hook.Add("Tick", "lag_detect", function()
 	local sys_time = SysTime()
 	local delta_time = sys_time-last_ticktime
+	pushFramerate(1/delta_time)
 	local not_from_hybernation = player.GetCount() > 0 -- GetCount doesn't count loading players.
-	if delta_time > threshold and ( sv_hibernate_think:GetBool() or not_from_hybernation ) and last_ticktime ~= 0 and not clean_up_started and game.MaxPlayers() ~= 1 then
+	if getAvarageFramerate() < threshold and ( sv_hibernate_think:GetBool() or not_from_hybernation ) and last_ticktime ~= 0 and not clean_up_started and game.MaxPlayers() ~= 1 then
+		clean_up_started = true
+		freezeAllPlayerProps()
 		RunConsoleCommand("phys_timescale", "0")
 		PrintMessage(HUD_PRINTTALK, notification)
 		local cur_time = CurTime()
+		Msg("Cleanup forced, dump of the last " .. tostring(num_frames) .. " frames:\n")
+		for i = 1, num_frames do
+			MsgN(last_frames[i])
+		end
 		FSBBroadcastTimer(cur_time, cur_time+seconds_before_cleanup, "timer.cleanup")
-		print("Frozen for:", sys_time-last_ticktime, "seconds")
 
 		timer.Simple(60, function()
 			game.CleanUpMap()
