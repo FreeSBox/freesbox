5ef8dcddcd8a1edac123e1ba767b5c3fc82792c1
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-10-30 20:30:13 +0500

    feat: Add custom names customname.

---
+++ lua/autorun/client/cl_nametags.lua
@@ -0,0 +1,142 @@
+-- https://gist.github.com/Earu/b437be27eb75780f108ac6cad58eecde#file-easychat_nametags-lua-L26
+
+local cl_nametags_enable = CreateClientConVar("cl_nametags_enable", "1", true)
+local cl_nametags_localplayer = CreateClientConVar("cl_nametags_localplayer", "1", true)
+
+surface.CreateFont("NameTagFont", { font = "Roboto Bold", extended = true, size = 180, weight = 880, blursize = 0.5, additive = false })
+surface.CreateFont("NameTagShadowFont", { font = "Roboto Bold", extended = true, size = 180, weight = 880, blursize = 5 })
+surface.CreateFont("AFKFont", { font = "Roboto Bold", extended = true, size = 100, weight = 880, blursize = 0.5, additive = false })
+surface.CreateFont("AFKShadowFont", { font = "Roboto Bold", extended = true, size = 100, weight = 880, blursize = 5 })
+
+local nametag = {
+	Nick = "",
+	DefaultColor = color_white,
+	Markup = nil,
+	NameTag = "",
+	MarkupTag = nil
+}
+
+function nametag:ParseNick(ply)
+	local team_col, nick, nametag = team.GetColor(ply:Team()), ply:RichName(), ply:GetNameTag()
+	if nick ~= self.Nick or team_col ~= self.DefaultColor or not self.Markup then
+		self.Markup = ec_markup.AdvancedParse(nick, {
+			nick = true,
+			default_color = team_col,
+			default_font = "NameTagFont",
+			default_shadow_font = "NameTagShadowFont",
+			shadow_intensity = 1,
+		})
+		self.DefaultColor = team_col
+		self.Nick = nick
+	end
+	if nametag ~= self.NameTag or not self.MarkupTag then
+		self.MarkupTag = ec_markup.AdvancedParse(nametag, {
+			nick = true,
+			default_color = color_white,
+			default_font = "AFKFont",
+			default_shadow_font = "AFKShadowFont",
+			shadow_intensity = 1,
+		})
+		self.NameTag = nametag
+	end
+end
+
+function nametag:Draw(ply)
+	self:ParseNick(ply)
+
+	self.Markup:SetPos(-self.Markup:GetWide() / 2, 0)
+	self.MarkupTag:SetPos(-self.MarkupTag:GetWide() / 2, 165)
+
+	--render.PushFilterMag(TEXFILTER.ANISOTROPIC)
+	--render.PushFilterMin(TEXFILTER.ANISOTROPIC)
+	self.Markup:Draw()
+	self.MarkupTag:Draw()
+	--render.PopFilterMag()
+	--render.PopFilterMin()
+end
+
+-- actual drawing after this
+local player_GetAll, ipairs, LocalPlayer = _G.player.GetAll, _G.ipairs, _G.LocalPlayer
+local IsValid, EyeAngles, Vector = _G.IsValid, _G.EyeAngles, _G.Vector
+
+local cam_Start3D2D, cam_End3D2D = _G.cam.Start3D2D, _G.cam.End3D2D
+local draw_SimpleText = _G.draw.SimpleText
+local table_copy = _G.table.Copy
+
+---@param ply Player
+local function shouldGetOverheadPos(ply)
+	local lp = LocalPlayer()
+	if lp == ply and ( not lp:ShouldDrawLocalPlayer() or not cl_nametags_localplayer:GetBool() ) then return false end
+	if ply:Crouching() then return false end
+	if ply:InVehicle() then return false end
+	if lp:GetPos():DistToSqr(ply:GetPos()) > 5000 * 5000 then return false end
+
+	return true
+end
+
+---@param ply Player
+local function getOverheadPos(ply)
+	if not shouldGetOverheadPos(ply) then return end
+
+	local bone = 6
+	local pos = ply:GetBonePosition(bone) or ply:EyePos()
+
+	if not ply:GetBoneName(bone):lower():find("head") and ply:GetBoneCount() >= bone then
+		for i = 1, ply:GetBoneCount() do
+			if ply:GetBoneName(i):lower():find("head") then
+				pos = ply:GetBonePosition(i)
+				bone = i
+			end
+		end
+	end
+
+	if not ply:Alive() then
+		local rag = ply:GetRagdollEntity()
+		if IsValid(rag) then
+			pos = rag:GetBonePosition(bone)
+		end
+	end
+
+	return pos
+end
+
+local function createNewNametag(ply)
+	local nt = table_copy(nametag)
+	ply.Nametag = nt
+end
+
+hook.Add("PostDrawTranslucentRenderables", "player_name_tags", function()
+	if not cl_nametags_enable:GetBool() then return end
+	for _, ply in ipairs(player_GetAll()) do
+		local pos = getOverheadPos(ply)
+		if pos then
+			local ang = EyeAngles()
+			ang:RotateAroundAxis(ang:Right(), 90)
+			ang:RotateAroundAxis(ang:Up(), -90)
+			local scale = ply:GetModelScale() * 0.03
+			scale = scale < 0.01 and 0.01 or scale > 5 and 5 or scale
+			cam_Start3D2D(pos + Vector(0, 0, 18), ang, scale)
+				-- create a nametag object for each player
+				if not ply.Nametag then
+					createNewNametag(ply)
+				end
+
+				ply.Nametag:Draw(ply)
+
+				local focus_loss_time = ply:GetFocusLossTime()
+				if focus_loss_time ~= 0 then
+					local cur_time = CurTime()
+					local afk_time = cur_time-focus_loss_time
+					local afk_time_table = string.FormattedTime(afk_time)
+					local text = ""
+					if afk_time_table.h == 0 then
+						text = string.format("[ AFK: %02i:%02i ]", afk_time_table.m, afk_time_table.s)
+					else
+						text = string.format("[ AFK: %02i:%02i:%02i ]", afk_time_table.h, afk_time_table.m, afk_time_table.s)
+					end
+					draw_SimpleText(text, "AFKFont", 6, -100, HSVToColor(cur_time*16, 1, 1), TEXT_ALIGN_CENTER)
+				end
+			cam_End3D2D()
+		end
+	end
+end)
---
+++ lua/autorun/sh_custom_name.lua
@@ -0,0 +1,90 @@
+-- Custom player names.
+
+---@class Player
+local PLAYER = FindMetaTable("Player")
+local orig_name = PLAYER.Name
+
+function PLAYER:GetOriginalName()
+	return orig_name(self)
+end
+
+-- Don't care about tags, easychat will hook this and fix them
+PLAYER.Name = function(self)
+	if not IsValid(self) then return "Invalid" end
+	local name = self:GetNWString("nickname")
+	if name:Trim() == "" then
+		return orig_name(self)
+	else
+		return name
+	end
+end
+PLAYER.GetName = PLAYER.Name
+PLAYER.Nick = PLAYER.Name
+
+function PLAYER:SetName(name)
+	if SERVER then
+		if #name > 100 then return end
+		self:SetPData("nickname", name)
+		self:SetNWString("nickname", name)
+	else
+		net.Start("change_nickname")
+			net.WriteString(name)
+		net.SendToServer()
+	end
+end
+
+---comment
+---@param name string
+function PLAYER:SetNameNoSave(name)
+	self:SetNWString("nickname", name)
+end
+
+------ Nametags ------
+
+function PLAYER:SetNameTag(name_tag)
+	if SERVER then
+		if #name_tag > 256 then return end
+		self:SetPData("nametag", name_tag)
+		self:SetNWString("nametag", name_tag)
+	else
+		net.Start("change_nametag")
+			net.WriteString(name_tag)
+		net.SendToServer()
+	end
+end
+function PLAYER:SetNameTagNoSave(name)
+	self:SetNWString("nametag", name)
+end
+if CLIENT then
+	-- Wrapper function for uyutniy compatibility
+	function setcustomtitle(nametag) LocalPlayer():SetNameTag(nametag) end
+end
+
+function PLAYER:GetNameTag()
+	return self:GetNWString("nametag")
+end
+
+------ Server code ------
+
+if SERVER then
+	hook.Add("PlayerAuthed", "apply_custom_name", function(ply, steamid, uniqueid)
+		ply:SetNWString("nickname", ply:GetPData("nickname"))
+		ply:SetNWString("nametag", ply:GetPData("nametag"))
+	end)
+
+	util.AddNetworkString("change_nametag")
+	net.Receive("change_nametag", function(len, ply)
+		local name_tag = net.ReadString()
+		if #name_tag > 100 then return end
+		ply:SetPData("nametag", name_tag)
+		ply:SetNWString("nametag", name_tag)
+	end)
+
+	util.AddNetworkString("change_nickname")
+	net.Receive("change_nickname", function(len, ply)
+		local new_name = net.ReadString()
+		if #new_name > 100 then return end
+		ply:SetPData("nickname", new_name)
+		ply:SetNWString("nickname", new_name)
+	end)
+end
