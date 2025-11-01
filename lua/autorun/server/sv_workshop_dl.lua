975d37182e1cc6b62ff19e2d49aecd17c247db65
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-01 15:26:36 +0500

    feat: Replace simfphys with glide.
    feat: Add 3D Stream Radio.

--- lua/autorun/server/sv_workshop_dl.lua
+++ lua/autorun/server/sv_workshop_dl.lua
@@ -3,10 +3,10 @@
 resource.AddWorkshop("160250458") -- Wiremod
 
 --#region Petitioned addons
-resource.AddWorkshop("771487490") -- [simfphys] LUA Vehicles - Base
-resource.AddWorkshop("831680603") -- [simfphys] Armed
+resource.AddWorkshop("3389728250") -- Glide
 resource.AddWorkshop("1360233031") -- Half-Life-SWEPs
 resource.AddWorkshop("2821862386") -- Simple Weapons
 resource.AddWorkshop("2821865508") -- Simple Weapons CSS
 resource.AddWorkshop("609412954") -- Recolorable CS:S Skins
+resource.AddWorkshop("246756300") -- 3D Stream Radio
 --#endregion
