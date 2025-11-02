6a2b0de9d98f0d7c4197349457ed40476d9cabff
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-03 00:03:19 +0500

    refactor!: Move to manual init.

---
+++ lua/autorun/sh_fsb_init.lua
@@ -0,0 +1,56 @@
+
+local shared_files = {
+	"fsb/sh_anti_noclip_pvp.lua",
+	"fsb/sh_custom_name.lua",
+	"fsb/sh_dont_tool_players.lua",
+	"fsb/sh_has_focus.lua",
+	"fsb/sh_localization.lua",
+	"fsb/sh_on_screen_timer.lua",
+	"fsb/sh_playermodels.lua",
+	"fsb/sh_resources.lua",
+	"fsb/sh_rules.lua",
+	"fsb/sh_spawnprotect.lua",
+}
+
+local server_files = {
+	"fsb/server/sv_anti_net_msg_spam.lua",
+	"fsb/server/sv_cleanup.lua",
+	"fsb/server/sv_gcc_exploit_radhat.lua",
+	"fsb/server/sv_infammo.lua",
+	"fsb/server/sv_lagdetect.lua",
+	"fsb/server/sv_performance.lua",
+	"fsb/server/sv_player_menu_v2.lua",
+	"fsb/server/sv_remove_restrictions.lua",
+	"fsb/server/sv_servershutdown.lua",
+	"fsb/server/sv_skyboxprotect.lua",
+	"fsb/server/sv_workshop_dl.lua",
+	"fsb/server/sv_weapon_drop.lua",
+}
+
+local client_files ={
+	"fsb/client/cl_concommands_v2.lua",
+	"fsb/client/cl_gcc_exploit_radhat.lua",
+	"fsb/client/cl_nametags.lua",
+	"fsb/client/cl_player_menu_v2.lua",
+	"fsb/client/cl_rules.lua",
+}
+
+for index, value in ipairs(shared_files) do
+		include(value)
+		AddCSLuaFile(value)
+end
+
+if SERVER then
+	for index, value in ipairs(server_files) do
+		include(value)
+	end
+	for index, value in ipairs(client_files) do
+		AddCSLuaFile(value)
+	end
+else
+	for index, value in ipairs(client_files) do
+		include(value)
+	end
+end
+
+print("FreeSBox initialized.")
