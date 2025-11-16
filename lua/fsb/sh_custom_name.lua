400d5ec1ad27d159d36ef47df61eb3a2b9823dfc
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-16 18:57:44 +0500

    feat(CustomNames): Support using SetPlayerNameNoSave on the client.
    This may also be slightly more optimised.

--- lua/fsb/sh_custom_name.lua
+++ lua/fsb/sh_custom_name.lua
@@ -1,5 +1,8 @@
 -- Custom player names.
 
+local MAX_NAME_LENGTH = 100
+local MAX_TAG_LENGTH = 150
+
 ---@class Player
 local PLAYER = FindMetaTable("Player")
 local orig_name = PLAYER.Name
@@ -12,7 +15,7 @@ end
 PLAYER.Name = function(self)
 	if not IsValid(self) then return "Invalid" end
 	local name = self:GetNWString("nickname")
-	if name:Trim() == "" then
+	if name == "" then
 		return orig_name(self)
 	else
 		return name
@@ -21,41 +24,72 @@ end
 PLAYER.GetName = PLAYER.Name
 PLAYER.Nick = PLAYER.Name
 
+---@param name string
 function PLAYER:SetPlayerName(name)
 	if self:IsGhostBanned() then return end
+
+	name = name:Trim()
+	if #name > MAX_NAME_LENGTH then return end
 	if SERVER then
-		if #name > 100 then return end
 		self:SetPData("nickname", name)
 		self:SetNWString("nickname", name)
 	else
 		net.Start("change_nickname")
+			net.WriteBool(true) -- should_save
 			net.WriteString(name)
 		net.SendToServer()
 	end
 end
 
----comment
 ---@param name string
 function PLAYER:SetPlayerNameNoSave(name)
-	self:SetNWString("nickname", name)
+	if self:IsGhostBanned() then return end
+
+	name = name:Trim()
+	if #name > MAX_NAME_LENGTH then return end
+
+	if SERVER then
+		self:SetNWString("nickname", name)
+	else
+		net.Start("change_nickname")
+			net.WriteBool(false) -- should_save
+			net.WriteString(name)
+		net.SendToServer()
+	end
 end
 
 ------ Nametags ------
 
 function PLAYER:SetNameTag(name_tag)
 	if self:IsGhostBanned() then return end
+
+	name_tag = name_tag:Trim()
+	if #name_tag > MAX_TAG_LENGTH then return end
+
 	if SERVER then
-		if #name_tag > 256 then return end
 		self:SetPData("nametag", name_tag)
 		self:SetNWString("nametag", name_tag)
 	else
 		net.Start("change_nametag")
+			net.WriteBool(true) -- should_save
 			net.WriteString(name_tag)
 		net.SendToServer()
 	end
 end
-function PLAYER:SetNameTagNoSave(name)
-	self:SetNWString("nametag", name)
+function PLAYER:SetNameTagNoSave(name_tag)
+	if self:IsGhostBanned() then return end
+
+	name_tag = name_tag:Trim()
+	if #name_tag > MAX_TAG_LENGTH then return end
+
+	if SERVER then
+		self:SetNWString("nametag", name_tag)
+	else
+		net.Start("change_nametag")
+			net.WriteBool(false) -- should_save
+			net.WriteString(name_tag)
+		net.SendToServer()
+	end
 end
 if CLIENT then
 	-- Wrapper function for uyutniy compatibility
@@ -80,18 +114,29 @@ if SERVER then
 
 	util.AddNetworkString("change_nametag")
 	net.Receive("change_nametag", function(len, ply)
-		local name_tag = net.ReadString()
-		if #name_tag > 100 then return end
-		ply:SetPData("nametag", name_tag)
+		if ply:IsGhostBanned() then return end
+
+		local should_save = net.ReadBool()
+		local name_tag = string.Trim(net.ReadString())
+		if #name_tag > MAX_TAG_LENGTH then return end
+
+		if should_save then
+			ply:SetPData("nametag", name_tag)
+		end
 		ply:SetNWString("nametag", name_tag)
 	end)
 
 	util.AddNetworkString("change_nickname")
 	net.Receive("change_nickname", function(len, ply)
 		if ply:IsGhostBanned() then return end
-		local new_name = net.ReadString()
-		if #new_name > 100 then return end
-		ply:SetPData("nickname", new_name)
+
+		local should_save = net.ReadBool()
+		local new_name = string.Trim(net.ReadString())
+		if #new_name > MAX_NAME_LENGTH then return end
+
+		if should_save then
+			ply:SetPData("nickname", new_name)
+		end
 		ply:SetNWString("nickname", new_name)
 	end)
 end
