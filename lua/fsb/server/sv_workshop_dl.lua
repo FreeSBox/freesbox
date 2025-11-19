5558be05a5be807bfed8f168f25a38ffc3ddc881
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-19 17:06:10 +0500

    feat: Comply with petition 180.

--- lua/fsb/server/sv_workshop_dl.lua
+++ lua/fsb/server/sv_workshop_dl.lua
@@ -5,16 +5,12 @@ resource.AddWorkshop("160250458") -- Wiremod
 --#region Petitioned addons
 resource.AddWorkshop("3389728250") -- Glide
 resource.AddWorkshop("3389795738") -- Glide // GTAV: Helicopters
-resource.AddWorkshop("1360233031") -- Half-Life-SWEPs
-resource.AddWorkshop("2821862386") -- Simple Weapons
-resource.AddWorkshop("2821865508") -- Simple Weapons CSS
 resource.AddWorkshop("609412954") -- Recolorable CS:S Skins
 resource.AddWorkshop("246756300") -- 3D Stream Radio
 resource.AddWorkshop("2675972006") -- Custom Loadout
 resource.AddWorkshop("3318060741") -- rRadio
 resource.AddWorkshop("104575630") -- Ragdoll Mover
 resource.AddWorkshop("109643223") -- 3D2D Textscreens
-resource.AddWorkshop("942316885") -- Half-Life: Opposing Force Weapon Pack
 resource.AddWorkshop("2840295308") -- Primitive
 resource.AddWorkshop("2458909924") -- Prop2Mesh
 resource.AddWorkshop("287012681") -- Track Assembly Tool
