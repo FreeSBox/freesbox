cf3df5cd5fb28cf26222ba6bf89f39923ddd023a
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-22 02:54:04 +0500

    fix(Weapon Drop): Check if we have a weapon before trying to drop it.

--- lua/fsb/server/sv_weapon_drop.lua
+++ lua/fsb/server/sv_weapon_drop.lua
@@ -2,16 +2,18 @@ local DESPAWN_DELAY = 60
 
 -- Source:
 -- https://wiki.facepunch.com/gmod/Player:DropWeapon#example
-concommand.Add( "+drop", function( ply )
-	if ( ply:IsValid() ) then
-		local weapon = ply:GetActiveWeapon()
-		ply:DropWeapon(weapon)
-		weapon.SpawnedOnGround = true
+concommand.Add("+drop", function(ply)
+	if not IsValid(ply) then return end
 
-		timer.Simple(DESPAWN_DELAY, function ()
-			if IsValid(weapon) and not IsValid(weapon:GetParent()) then
-				weapon:Remove()
-			end
-		end)
-	end
-end )
+	local weapon = ply:GetActiveWeapon()
+	if not IsValid(weapon) then return end
+
+	ply:DropWeapon(weapon)
+	weapon.SpawnedOnGround = true
+
+	timer.Simple(DESPAWN_DELAY, function ()
+		if IsValid(weapon) and not IsValid(weapon:GetParent()) then
+			weapon:Remove()
+		end
+	end)
+end)
