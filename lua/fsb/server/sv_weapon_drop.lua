8ff68bdc6d81c820f6804e9508b2426388a54d79
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-22 02:48:16 +0500

    feat(Weapon Drop): Add despawn delay.
    This should also fix dropping weapons at build players.

--- lua/fsb/server/sv_weapon_drop.lua
+++ lua/fsb/server/sv_weapon_drop.lua
@@ -1,7 +1,17 @@
+local DESPAWN_DELAY = 60
+
 -- Source:
 -- https://wiki.facepunch.com/gmod/Player:DropWeapon#example
 concommand.Add( "+drop", function( ply )
 	if ( ply:IsValid() ) then
-		ply:DropWeapon()
+		local weapon = ply:GetActiveWeapon()
+		ply:DropWeapon(weapon)
+		weapon.SpawnedOnGround = true
+
+		timer.Simple(DESPAWN_DELAY, function ()
+			if IsValid(weapon) and not IsValid(weapon:GetParent()) then
+				weapon:Remove()
+			end
+		end)
 	end
 end )
