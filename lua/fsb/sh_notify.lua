c45b648db40a08c44ee36243b5fe77d82c466544
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-23 02:19:33 +0500

    fix(Notify): Add NOTIFY enum to the server.

--- lua/fsb/sh_notify.lua
+++ lua/fsb/sh_notify.lua
@@ -1,6 +1,12 @@
 if SERVER then
 	util.AddNetworkString("SendLocalizedHint")
 
+	NOTIFY_GENERIC = 0
+	NOTIFY_ERROR = 1
+	NOTIFY_UNDO	= 2
+	NOTIFY_HINT	= 3
+	NOTIFY_CLEANUP	= 4
+
 	---@class Player
 	local PLAYER = FindMetaTable("Player")
 
