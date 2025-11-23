64426609378535ca9182eab8480ab4deab612788
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-11-24 00:53:57 +0500

    fix(E2): Use numbers instead of booleans.

--- lua/entities/gmod_wire_expression2/core/custom/fsb.lua
+++ lua/entities/gmod_wire_expression2/core/custom/fsb.lua
@@ -6,14 +6,14 @@ e2function number entity:isBUILD()
 	if not IsValid(this) then return self:throw("Invalid entity!", "") end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got an Entity!", "") end
 
-	return not this:InPVPMode()
+	if this:InPVPMode() then return 0 else return 1 end
 end
 
 e2function number entity:isPVP()
 	if not IsValid(this) then return self:throw("Invalid entity!", "") end
 	if not this:IsPlayer() then return self:throw("Expected a Player but got an Entity!", "") end
 
-	return this:InPVPMode()
+	if this:InPVPMode() then return 1 else return 0 end
 end
 
 e2function number entity:pvpModeEndTime()
