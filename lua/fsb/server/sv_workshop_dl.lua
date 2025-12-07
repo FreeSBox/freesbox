d4086599142d50dd081e7fc8d87d7ebc63b68cee
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-12-07 15:32:48 +0500

    chore(Workshop): Replace simple-weapons with acxion.

--- lua/fsb/server/sv_workshop_dl.lua
+++ lua/fsb/server/sv_workshop_dl.lua
@@ -20,11 +20,10 @@ resource.AddWorkshop("3457996478") -- Glide // Conscript APC
 resource.AddWorkshop("3436601554") -- Glide // Hunter-Chopper
 resource.AddWorkshop("3448521354") -- Glide // Combine Gunship
 resource.AddWorkshop("3001397905") -- Media Player (Updated Edition)
-resource.AddWorkshop("2821862386") -- Simple Weapons: Base
-resource.AddWorkshop("2821865508") -- Simple Weapons: CS:S
-resource.AddWorkshop("2832566843") -- Simple Weapons: Half-Life 2 Extended
 resource.AddWorkshop("3081978414") -- Profiteers Parachute
 resource.AddWorkshop("2825347854") -- GPoker - Poker in Garry's Mod
 resource.AddWorkshop("2718789320") -- QCardWars - Card Game in GMod!
 resource.AddWorkshop("3542961565") -- Liberty City // GLIDE
+resource.AddWorkshop("3180160649") -- ACXION
+resource.AddWorkshop("2704542243") -- GTA IV & EFLC Weapons & Magazines Models Pack
 --#endregion
