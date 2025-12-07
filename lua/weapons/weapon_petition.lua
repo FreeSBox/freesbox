b60988bc0d04ba0575d91d716a5faf1101771eb8
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-12-08 02:37:29 +0500

    fix(Petition SWEP): Add missing function name.
    How did this even happen?

--- lua/weapons/weapon_petition.lua
+++ lua/weapons/weapon_petition.lua
@@ -186,7 +186,7 @@ local function draw3DPetition(petition, new_pos, new_ang, scale, font, draw_fine
 		--draw.DrawText( , "Default", cam_size_x/2, 30, color_black, TEXT_ALIGN_CENTER )
 		local y_len = drawMultiLine(petition.name, font, size_x, size_y/14, size_x/2, size_y/16, color_black, TEXT_ALIGN_CENTER)
 		if draw_fineprint then
-			draw.DrawText(()"petition.open_hint"), "Default", size_x/2, size_y-32, color_black, TEXT_ALIGN_CENTER)
+			draw.DrawText(FSB.Translate("petition.open_hint"), "Default", size_x/2, size_y-32, color_black, TEXT_ALIGN_CENTER)
 		end
 	cam.End3D2D()
 end
