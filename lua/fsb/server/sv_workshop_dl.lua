d814b632e513542e7f6e90f25c5e76b229ad1308
tupoy-ya <tupoy-ya@users.noreply.github.com>
2025-12-17 02:01:52 +0500

    fix(Workshop): Remove some useless addons.

--- lua/fsb/server/sv_workshop_dl.lua
+++ lua/fsb/server/sv_workshop_dl.lua
@@ -30,8 +30,6 @@ resource.AddWorkshop("2718789320") -- QCardWars - Card Game in GMod!
 resource.AddWorkshop("3542961565") -- Liberty City // GLIDE
 resource.AddWorkshop("104691717") -- PAC3
 resource.AddWorkshop("3561915009") -- Glide Handheld Weapons
-resource.AddWorkshop("950845673") -- Parakeet's Pill Pack
-resource.AddWorkshop("872114763") -- Fazbear's Ultimate Pill Pack Remaster
 resource.AddWorkshop("2989564194") -- Simple Weapons: by Doge
 resource.AddWorkshop("668552230") -- HBOMBS Base Pack
 resource.AddWorkshop("668558959") -- HBOMBS Materials Pack
