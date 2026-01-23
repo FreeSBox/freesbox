6c835cc54441e0118dbf857203a1f17f91cefdbd
tupoy-ya <tupoy-ya@users.noreply.github.com>
2026-01-23 23:46:39 +0500

    chore(Workshop): Add more useless addons.

--- lua/fsb/server/sv_workshop_dl.lua
+++ lua/fsb/server/sv_workshop_dl.lua
@@ -33,4 +33,5 @@ resource.AddWorkshop("2538387266") -- ActMod
 resource.AddWorkshop("2615656036") -- Base Anim-AM4
 resource.AddWorkshop("3567487307") -- [ActMod] More Emotes
 resource.AddWorkshop("104548572") -- Playable Piano
+resource.AddWorkshop("2953413221") -- Barricade SWEP
 --#endregion
