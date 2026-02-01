5e6696befbb1eee1ebf432916cf4a2394b39a23d
tupoy-ya <tupoy-ya@users.noreply.github.com>
2026-02-02 01:00:48 +0500

    feat: Added `findchips` command.

--- lua/fsb/server/sv_findlag.lua
+++ lua/fsb/server/sv_findlag.lua
@@ -2,6 +2,14 @@
 local ratelimit = 5
 local ratelimit_table = {}
 
+local function cmd_out(ply, msg)
+	if IsValid(ply) then
+		ply:PrintMessage(HUD_PRINTCONSOLE, msg)
+	else
+		MsgN(msg)
+	end
+end
+
 concommand.Add("findlag", function (ply, cmd, args, arg_str)
 	if not FSB.Ratelimit(ratelimit_table, ply, 5) then
 		ply:SendLocalizedMessage("ratelimit", ratelimit)
@@ -58,12 +66,53 @@ concommand.Add("findlag", function (ply, cmd, args, arg_str)
 
 	if IsValid(ply) then
 		ply:SendLocalizedMessage("findlag.note")
-		for _, index in ipairs(sorted_scores) do
-			ply:PrintMessage(HUD_PRINTTALK, string.format("%s: %i", index:Nick(), math.floor(lag_scores[index])))
+	end
+	for _, index in ipairs(sorted_scores) do
+		cmd_out(ply, string.format("%s: %i", index:Nick(), math.floor(lag_scores[index])))
+	end
+end)
+
+---@param chip Entity
+local function getChipDataForPrint(chip)
+	local is_e2 = chip:GetClass() == "gmod_wire_expression2"
+
+	local name
+	local exec_time
+	local chip_type = is_e2 and "E2" or "SF"
+
+	if is_e2 then
+		name = chip.name
+		exec_time = chip.context.timebench*1000
+	else -- Assume starfall
+		name = chip.name
+		exec_time = chip.instance.cpu_average*1000
+	end
+
+	local owner = chip:CPPIGetOwner()
+	local owner_text = IsValid(owner) and owner:Nick() or "Disconnected/World"
+
+	return string.format("| %-24s | %s | %-32s | %.1fms	|", owner_text, chip_type, name, exec_time)
+end
+
+concommand.Add("findchips", function (ply, cmd, args, arg_str)
+	if not FSB.Ratelimit(ratelimit_table, ply, 5) then
+		ply:SendLocalizedMessage("ratelimit", ratelimit)
+		return
+	end
+
+	local chips = {}
+
+	for _, entity in ipairs(ents.GetAll()) do
+		if entity:GetClass() == "gmod_wire_expression2" then
+			chips[#chips+1] = entity
 		end
-	else
-		for _, index in ipairs(sorted_scores) do
-			print(string.format("%s: %i", index:Nick(), math.floor(lag_scores[index])))
+
+		if entity:GetClass() == "starfall_processor" then
+			chips[#chips+1] = entity
 		end
+	end
+
+	for _, chip in ipairs(chips) do
+		cmd_out(ply, getChipDataForPrint(chip))
 	end
 end)
