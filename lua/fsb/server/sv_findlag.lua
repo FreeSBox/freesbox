89aea773d761adda37eda3062c9eeefdf3fe5053
tupoy-ya <tupoy-ya@users.noreply.github.com>
2026-02-19 03:19:04 +0500

    feat(Findlag): Add support for wire FPGA.

--- lua/fsb/server/sv_findlag.lua
+++ lua/fsb/server/sv_findlag.lua
@@ -10,6 +10,14 @@ local function cmd_out(ply, msg)
 	end
 end
 
+local function getSFCPUAverage(chip)
+	if chip.instance.perf ~= nil then
+		return chip.instance.perf.cpuAverage
+	else -- Support older versions of starfall.
+		return chip.instance.cpu_average
+	end
+end
+
 concommand.Add("findlag", function (ply, cmd, args, arg_str)
 	if not FSB.Ratelimit(ratelimit_table, ply, RATELIMIT) then
 		ply:SendLocalizedMessage("ratelimit", RATELIMIT)
@@ -24,6 +32,8 @@ concommand.Add("findlag", function (ply, cmd, args, arg_str)
 		if not IsValid( owner ) then
 			continue
 		end
+
+		local class = entity:GetClass()
 
 		local frozen = true
 		for i = 0, entity:GetPhysicsObjectCount()-1 do
@@ -44,22 +54,17 @@ concommand.Add("findlag", function (ply, cmd, args, arg_str)
 			ent_score = ent_score + #entity.Constraints * 0.5
 		end
 
-		if entity:GetClass() == "gmod_wire_gate" then
+		if class == "gmod_wire_gate" then
 			ent_score = ent_score + 1
-		end
-
-		if entity:GetClass() == "gmod_wire_expression2" then
-			local data = entity:GetOverlayData()
-			if not data then continue end
-			if not data.context then continue end
-
-			ent_score = ent_score + data.context.timebench*2000
-		end
-
-		if entity:GetClass() == "starfall_processor" then
+		elseif class == "gmod_wire_expression2" then
+			if not entity.context then continue end
+			ent_score = ent_score + entity.context.timebench*2000
+		elseif class == "gmod_wire_fpga" then
+			if not entity.timebench then continue end
+			ent_score = ent_score + entity.timebench*2000
+		elseif class == "starfall_processor" then
 			if not entity.instance then continue end
-
-			ent_score = ent_score + entity.instance.perf.cpuAverage*2000
+			ent_score = ent_score + getSFCPUAverage(entity)*2000
 		end
 
 		lag_scores[owner] = lag_scores[owner] and lag_scores[owner] + ent_score or ent_score
@@ -75,27 +80,62 @@ concommand.Add("findlag", function (ply, cmd, args, arg_str)
 	end
 end)
 
----@param chip Entity
-local function getChipDataForPrint(chip)
-	local is_e2 = chip:GetClass() == "gmod_wire_expression2"
-
-	local name = "errored"
+local function getE2DataForPrint(chip, owner_name)
 	local exec_time = 0
-	local chip_type = is_e2 and "E2" or "SF"
+	local name = tostring(chip.name)
 
-	if is_e2 and chip.context ~= nil then
-		name = chip.name
+	if chip.context ~= nil then
 		exec_time = chip.context.timebench*1000
-	elseif chip.instance ~= nil then -- Assume starfall
-		name = chip.name
-		exec_time = chip.instance.perf.cpuAverage*1000
 	end
 
-	local owner = chip:CPPIGetOwner()
-	local owner_text = IsValid(owner) and owner:Nick() or "Disconnected/World"
-
-	return string.format("| %-24s | %s | %-32s | %.1fms	|", owner_text, chip_type, name, exec_time)
+	return string.format("| %-24s | E2    | %-32s | %.1fms	|", owner_name, name, exec_time)
 end
+
+local function getFPGADataForPrint(chip, owner_name)
+	local exec_time = 0
+	local name = tostring(chip.name)
+
+	if chip.timebench ~= nil then
+		exec_time = chip.timebench*1000
+	end
+
+	return string.format("| %-24s | FPGA  | %-32s | %.1fms	|", owner_name, name, exec_time)
+end
+
+local function getSFDataForPrint(chip, owner_name)
+	local exec_time = 0
+	local name = chip.name and tostring(chip.name) or "errored"
+
+	if chip.instance ~= nil then
+		exec_time = getSFCPUAverage(chip)
+	end
+
+	return string.format("| %-24s | SF    | %-32s | %.1fms	|", owner_name, name, exec_time)
+end
+
+---@param chip Entity
+local function getChipDataForPrint(chip)
+	local owner = chip:CPPIGetOwner()
+	local owner_name = IsValid(owner) and owner:Nick() or "Disconnected/World"
+	local class = chip:GetClass()
+
+	if class == "gmod_wire_expression2" then
+		return getE2DataForPrint(chip, owner_name)
+	elseif class == "gmod_wire_fpga" then
+		return getFPGADataForPrint(chip, owner_name)
+	elseif class == "starfall_processor" then
+		return getSFDataForPrint(chip, owner_name)
+	end
+
+	return "!invalid chip!"
+end
+
+local chip_classes =
+{
+	["gmod_wire_expression2"] = true,
+	["gmod_wire_fpga"] = true,
+	["starfall_processor"] = true,
+}
 
 concommand.Add("findchips", function (ply, cmd, args, arg_str)
 	if not FSB.Ratelimit(ratelimit_table, ply, RATELIMIT) then
@@ -106,11 +146,7 @@ concommand.Add("findchips", function (ply, cmd, args, arg_str)
 	local chips = {}
 
 	for _, entity in ipairs(ents.GetAll()) do
-		if entity:GetClass() == "gmod_wire_expression2" then
-			chips[#chips+1] = entity
-		end
-
-		if entity:GetClass() == "starfall_processor" then
+		if chip_classes[entity:GetClass()] then
 			chips[#chips+1] = entity
 		end
 	end
