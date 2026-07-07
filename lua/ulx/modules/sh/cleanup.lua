c07799809472e355bfe0b1d7a5a8558f03fafcb0
tupoy-ya <tupoy-ya@users.noreply.github.com>
2026-07-07 14:24:42 +0500

    feat(ULX): Add votecleanup command.

--- lua/ulx/modules/sh/cleanup.lua
+++ lua/ulx/modules/sh/cleanup.lua
@@ -50,6 +50,48 @@ local function freezePlayerProps(calling_ply, target_ply)
 	ulx.fancyLogAdmin(calling_ply, "#A froze #T's props", target_ply)
 end
 
+
+local function getVoteResultsStr(success_votes, num_votes, ratio_needed)
+	return string.format("(%i/%i, needed %i%%)", success_votes or 0, num_votes or 0, ratio_needed*100)
+end
+
+local function cleanupVoteCallback(t)
+	local winner
+	local winner_votes = 0
+	for id, num_votes in pairs(t.results) do
+		if num_votes > winner_votes then
+			winner = id
+			winner_votes = num_votes
+		end
+	end
+
+	local TIME_UNTIL_CLEANUP = 60
+	local RATIO_NEEDED = 0.6
+	local print_text
+	if winner ~= 1 or winner_votes / t.voters < RATIO_NEEDED then
+		print_text = "Map will not be cleaned up. " .. getVoteResultsStr(t.results[1], t.voters, RATIO_NEEDED)
+	else
+		print_text = "Map will be cleaned up in " .. TIME_UNTIL_CLEANUP .. " seconds. " .. getVoteResultsStr(t.results[1], t.voters, RATIO_NEEDED)
+		FSB.CleanUpMap(TIME_UNTIL_CLEANUP)
+	end
+
+	ULib.tsay(nil, print_text)
+	ulx.logString(print_text)
+	MsgN(print_text)
+
+end
+
+local function cleanupVote(calling_ply)
+	if ulx.voteInProgress then
+		ULib.tsayError(calling_ply, "There is already a vote in progress. Please wait for the current one to end.", true)
+		return
+	end
+
+	ulx.doVote("Cleanup map?", {"Yes", "No"}, cleanupVoteCallback, 20, nil, false, calling_ply)
+
+	ulx.fancyLogAdmin(calling_ply, "#A started a vote for cleanup")
+end
+
 local cleanupplayer = ulx.command("FSB", "ulx cleanplayer", cleanupplayerULXWrapper, "!cleanplayer")
 cleanupplayer:addParam{ type=ULib.cmds.PlayerArg }
 cleanupplayer:defaultAccess( ULib.ACCESS_ADMIN )
@@ -67,6 +109,10 @@ cleanup:help( "Cleans up the map with a visual timer." )
 local stopcleanup = ulx.command("FSB", "ulx stopcleanup", cancelCleanupULXWrapper, "!stopcleanup")
 stopcleanup:defaultAccess( ULib.ACCESS_ADMIN )
 stopcleanup:help( "Cancels the cleanup if started." )
+
+local votecleanup = ulx.command("FSB", "ulx votecleanup", cleanupVote, "!votecleanup")
+votecleanup:defaultAccess( ULib.ACCESS_ALL )
+votecleanup:help( "Starts a vote for map clean up." )
 
 local freezeall = ulx.command("FSB", "ulx freezeall", freezeAllULXWrapper, "!freezeall")
 freezeall:defaultAccess( ULib.ACCESS_ADMIN )
