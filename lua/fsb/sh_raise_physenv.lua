be745a625f114102182c365bd00c336ab8b1fb44
thebashforkbomber <102991955+thebashforkbomber@users.noreply.github.com>
2026-08-10 15:56:18 +0500

    feat(physenv): raise limits (#38)
    * feat(physenv): raise limits
    
    * chore: Fix indentation.
    
    ---------
    
    Co-authored-by: tupoy-ya <72797377+tupoy-ya@users.noreply.github.com>

---
+++ lua/fsb/sh_raise_physenv.lua
@@ -0,0 +1,12 @@
+-- Petition #1316
+-- This uses literally the same code with the print function removed, although it seems correct
+hook.Add("Think", "init_phys_perf", function()
+	hook.Remove("Think", "init_phys_perf")
+	local TAB = physenv.GetPerformanceSettings()
+
+	TAB.MaxFrictionMass = 1000000
+	TAB.MaxVelocity = 1000000
+	TAB.MaxAngularVelocity = 1000000
+
+	physenv.SetPerformanceSettings(TAB)
+end)
