83e2692fef075be6d7bf1de06004e8639bd347b8
thebashforkbomber <102991955+thebashforkbomber@users.noreply.github.com>
2026-08-13 10:52:14 +0000

    feat(sf):Add playtime tracking function
    Whoops, turns out I didn't need that metatable shit.

--- lua/starfall/libs_sh/fsb.lua
+++ lua/starfall/libs_sh/fsb.lua
@@ -3,7 +3,7 @@ local ENT_META = FindMetaTable("Entity")
 ---@class Player
 local PLY_META = FindMetaTable("Player")
 
-local Ply_GetGhostBanDescription, Ply_GetGhostBannedBySteamID64, Ply_IsGhostBanned, Ply_InPVPMode, Ply_GetOriginalName, Ply_PVPModeEndTime, Ply_GetNameTag, Ply_GetPlaytime = PLY_META.GetGhostBanDescription, PLY_META.GetGhostBannedBySteamID64, PLY_META.IsGhostBanned, PLY_META.InPVPMode, PLY_META.GetOriginalName, PLY_META.PVPModeEndTime, PLY_META.GetNameTag, PLY_META.GetPlayTime
+local Ply_GetGhostBanDescription, Ply_GetGhostBannedBySteamID64, Ply_IsGhostBanned, Ply_InPVPMode, Ply_GetOriginalName, Ply_PVPModeEndTime, Ply_GetNameTag = PLY_META.GetGhostBanDescription, PLY_META.GetGhostBannedBySteamID64, PLY_META.IsGhostBanned, PLY_META.InPVPMode, PLY_META.GetOriginalName, PLY_META.PVPModeEndTime, PLY_META.GetNameTag
 local Ent_IsValid = ENT_META.IsValid
 local add = SF.hookAdd
 
@@ -29,7 +29,7 @@ add("FSBPlayerChangeNameTag")
 
 return function(instance)
 local player_methods, player_meta = instance.Types.Player.Methods, instance.Types.Player
-
+-- Internal validity check just for SF. Returns ent if it's valid, errors the chip if it's not. Use this instead of just directly using self.
 local function getply(self)
 	local ent = player_meta.sf2sensitive[self]
 	if Ent_IsValid(ent) then
@@ -98,10 +98,10 @@ end
 function player_methods:getNameTag()
 	return Ply_GetNameTag(getply(self))
 end
--- Returns the amount of hours the player has played on the server.
+--- Returns the total amount of time the player has played on the server.
 -- @server
--- @return number Playtime hours.
-function player_methods:getPlaytime()
-	return Ply_GetPlaytime(getply(self))
+-- @return number Playtime.
+function player_methods:getTotalPlaytime()
+	return getply(self):GetUTimeTotalTime()
 end
 end
